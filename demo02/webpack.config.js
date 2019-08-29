4d28802017c32b6a15573c4e512961d63fe48bae
leecason <leecason0722@gmail.com>
2019-08-29 12:14:52 +0800

    ✨: demo02

---
+++ demo02/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]_[hash:8].js',
+  },
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all'
+    },
+  },
+};
